ba8bc0bfa49bd013e20abc06a476937e59519ea5
João Mauricio <joaomauricio.novigoit@fujioka.inf.br>
2026-03-08 17:42:02 -0300

    feat: Add CouponController for managing coupon creation and listing

---
+++ backend/src/main/java/com/joaomauriciodev/coupons/controller/CouponController.java
@@ -0,0 +1,34 @@
+package com.joaomauriciodev.coupons.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaomauriciodev.coupons.model.Coupon;
+import com.joaomauriciodev.coupons.service.CouponService;
+
+@RestController
+@RequestMapping("/coupons")
+public class CouponController {
+
+    private final CouponService couponService;
+
+    public CouponController(CouponService couponService){
+        this.couponService = couponService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Coupon> create(@RequestBody Coupon coupon){
+        return ResponseEntity.ok(couponService.createCoupon(coupon));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Coupon>> list(){
+        return ResponseEntity.ok(couponService.list());
+    }
+}
